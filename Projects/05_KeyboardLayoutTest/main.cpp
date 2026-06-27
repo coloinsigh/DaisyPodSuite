3d186bdb4c96b7480e41090cb713cb092fdaa511
coloinsigh <coulomb.lynch@gmail.com>
2026-06-28 00:15:16 +0100

    Break multiple buttons out into different oscillators and envelopes

--- Projects/05_KeyboardLayoutTest/main.cpp
+++ Projects/05_KeyboardLayoutTest/main.cpp
@@ -21,6 +21,18 @@ Voice voice_pool[NUM_VOICES];
 
 float target_freq = 440.0;
 
+// Define a LUT containing 8 notes in C
+const float SCALE_LUT[NUM_VOICES] = {
+    261.63f, // C4
+    293.66f, // D4
+    329.63f, // E4
+    349.23f, // F4
+    392.00f, // G4
+    440.00f, // A4
+    493.88f, // B4
+    523.25f  // C5
+};
+
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size){
     for (size_t i = 0; i < size; i++){
@@ -43,5 +55,16 @@ int main(void) {
 
     // Initializae oscillator
     float sample_rate = hw.AudioSampleRate();
-    osc.Init(sample_rate);
+    for (int x = 0; x < NUM_VOICES; x++){
+        // Initialize Oscillator for each button
+        voice_pool[x].osc.Init(sample_rate);
+        voice_pool[x].osc.SetWaveform(Oscillator::WAVE_TRI);
+        voice_pool[x].osc.SetFreq(SCALE_LUT[x]);
+
+        // Initialize envelope for each button
+        voice_pool[x].env.Init(sample_rate);
+        voice_pool[x].env.SetTime(ADENV_SEG_ATTACK, 0.05f); // attack
+        voice_pool[x].env.SetTime(ADENV_SEG_DECAY, 0.4f);   // decay
+        voice_pool[x].env.SetMax(1.0f);                     // max volume
+    }
 }
